77833e75af91880b2ca69f8f89450e22a209b11a
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-08-06 15:06:34 +0330

    migrated User to Profile

---
+++ core/accounts/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User,Profile
+
+
+
+class CustomUserAdmin(UserAdmin):
+    #form
+    #add_form
+    model = User
+    list_display = ('email','is_superuser','is_active',)
+    list_filter = ('email','is_superuser','is_active',)
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active","is_superuser")}),
+        ("Group Permissions", {"fields": ("groups", "user_permissions",)}),
+        ("Important Data", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "is_superuser",
+            )}
+        ),
+    )
+
+
+
+admin.site.register(User,CustomUserAdmin)
+admin.site.register(Profile)
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 # Getting user model here 
 User = get_user_model()
 
