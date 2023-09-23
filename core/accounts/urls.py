cbf6652e224b423e955261579479ae1941eb888f
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-23 12:45:49 +0330

    add api/v2 authentication using djoser

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -5,5 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
-    path('api/v1/',include('accounts.api.v1.urls')),
+    # path('api/v1/',include('accounts.api.v1.urls')),
+    path('api/v2/', include('djoser.urls')),
 ]
