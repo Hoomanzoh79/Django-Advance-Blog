3ad514a1316bb751c6318507bad5fe4a8fac5ca9
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-25 11:08:17 +0330

    add my first test

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse,resolve
+from .views import IndexView
 
-# Create your tests here.
+
+class TestUrl(TestCase):
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:index')
+        self.assertEquals(resolve(url).func.view_class,IndexView)
