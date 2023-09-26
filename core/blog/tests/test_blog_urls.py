a6a343c732212398f4720178bbaee45909e86d36
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-26 10:33:37 +0330

    change TestCase to SimpleTestCase

--- core/blog/tests/test_blog_urls.py
+++ core/blog/tests/test_blog_urls.py
@@ -3,7 +3,7 @@ from django.urls import reverse,resolve
 from blog.views import IndexView,PostListView,PostDetailView
 
 
-class TestUrl(TestCase):
+class TestUrl(SimpleTestCase):
     def test_blog_index_url_resolve(self):
         url = reverse('blog:index')
         self.assertEquals(resolve(url).func.view_class,IndexView)
