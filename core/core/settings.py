280f0e4242e5325cc8aa915c3edec56390ba7108
Hooman Zohrevand <hoomanemi1999@gmail.com>
2023-09-28 11:11:42 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# 'django-insecure-ki2^hwx-6r*r@^r^f$h@v**u0ax%6d-m+4(%phhie1^+xwnph5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
