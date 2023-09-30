9ade7de9326e185b608c026e9fb176caa8317262
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-09-30 12:21:58 +0330

    add some fixtures with pytest

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -2,17 +2,29 @@ import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
 from datetime import datetime
+from accounts.models import User
+
+
+@pytest.fixture
+def api_client():
+    client = APIClient()
+    return client
+
+@pytest.fixture
+def test_user():
+    user = User.objects.create_user(email='test@test.com',
+                                    password='Alishab13%',
+                                    is_verified=True,)
+    return user   
 
 @pytest.mark.django_db
 class TestPostApi:
-    client = APIClient()
-
-    def test_get_post_response_200_status(self):
+    def test_get_post_response_200_status(self,api_client):
         url = reverse('blog:api-v1:post-list')
-        response = self.client.get(url)
+        response = api_client.get(url)
         assert response.status_code == 200
     
-    def test_create_post_response_401_status(self):
+    def test_create_post_response_401_status(self,api_client):
         url = reverse('blog:api-v1:post-list')
         data = {
             'title':'test title',
@@ -20,5 +32,5 @@ class TestPostApi:
             'status':True,
             'published_data':datetime.now(),
         }
-        response = self.client.post(url,data=data)
+        response = api_client.post(url,data=data)
         assert response.status_code == 401
