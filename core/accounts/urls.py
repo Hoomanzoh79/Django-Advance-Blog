868d93b103e392f0e4d78ffb7a86f97605831865
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-10-13 12:50:27 +0330

    add jwt to djoser

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     # path('api/v1/',include('accounts.api.v1.urls')),
     path("api/v2/", include("djoser.urls")),
+    path("api/v2/", include("djoser.urls.jwt")),
 ]
