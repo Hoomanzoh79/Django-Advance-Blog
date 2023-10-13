c6da2b50302d9b6afc229a800c1866fe074aa740
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-10-13 13:06:52 +0330

    add locustfile

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,4 +1,18 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task
 
 class QuickstartUser(HttpUser):
-    pass
+
+    def on_start(self):
+        response = self.client.post('/accounts/api/v2/jwt/create/',data={
+              "email": "admin@admin.com",
+              "password": "Alishab13",
+        }).json()
+        self.client.headers = {"Authorization": f"Bearer {response.get('access',None)}"}
+    
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
+    
+    @task
+    def category_list(self):
+        self.client.get("/blog/api/v1/category/")
