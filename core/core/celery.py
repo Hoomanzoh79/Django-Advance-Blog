58ac34d980d48e2a258f35c9fb9c380040c67641
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-11-02 11:17:01 +0330

    delete some comments

--- core/core/celery.py
+++ core/core/celery.py
@@ -15,11 +15,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-
-# (2nd way of configuring periodic tasks)
-
-# from accounts.tasks import sendEmail
-# @app.on_after_configure.connect
-# def setup_periodic_tasks(sender, **kwargs):
-#     sender.add_periodic_task(10.0, sendEmail.s(), name='send email every 10 seconds')
