69e36c2b53cd81bbeb16188ceeea76187b45217a
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-11-12 10:19:00 +0330

    add nginx and gunicorn

--- core/core/settings.py
+++ core/core/settings.py
@@ -96,9 +96,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
