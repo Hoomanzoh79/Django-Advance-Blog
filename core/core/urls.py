ddbc28cf53d628c6e11aa1c3a545abdf80204bad
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-11-16 11:52:05 +0330

    reformat files

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.documentation import include_docs_urls
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
